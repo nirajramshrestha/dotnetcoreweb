b2376cacd6290b2f081e586898cce447c3f8bf0c
niraj.shrestha <niraj.shrestha@eplus.com>
2020-01-30 13:26:21 -0500

    'update'

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace dotnetcoreweb
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Niraj");
             });
         }
     }
